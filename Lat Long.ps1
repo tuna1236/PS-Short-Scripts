67d7193da3f15abbebcd14251ca62e4dffe334d1
Cole <tuna1236@gmail.com>
2023-02-27 09:29:25 -0800

    Create Lat Long.ps1

---
+++ Lat Long.ps1
@@ -0,0 +1,13 @@
+#Locatrion scriopt
+
+	while (($GeoWatcher.Status -ne 'Ready') -and ($GeoWatcher.Permission -ne 'Denied')) {
+		Start-Sleep -Milliseconds 100 #Wait for discovery.
+	}  
+
+	if ($GeoWatcher.Permission -eq 'Denied'){
+		Write-Error 'Access Denied for Location Information'
+	} else {
+		$GeoWatcher.Position.Location | Select Latitude,Longitude #Select the relevent results.
+		$lat = $GeoWatcher.Position.Location.Latitude
+		$long =  $GeoWatcher.Position.Location.Longitude
+	}
