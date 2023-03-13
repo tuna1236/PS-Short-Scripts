a95295f0b36a5c00a57f8e757638d5c377e6cf54
Cole <tuna1236@gmail.com>
2023-03-13 16:23:42 -0700

    Update Change Network.ps1

--- Change Network.ps1
+++ Change Network.ps1
@@ -1,2 +1,5 @@
 Get-NetConnectionProfile
 Set-NetConnectionProfile -InterfaceIndex <index number> -NetworkCategory Private
+
+
+Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles
