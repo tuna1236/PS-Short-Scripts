1fcbd6fdc13a468e2fbb05855a92027c3a8fd3f7
Cole <tuna1236@gmail.com>
2023-03-17 12:14:36 -0700

    Update Dell Command Update Config.ps1

--- Dell Command Update Config.ps1
+++ Dell Command Update Config.ps1
@@ -1,3 +1,5 @@
 #Set Configuration
 Set-Location -Path "C:\Program Files (x86)\Dell\CommandUpdate"
 .\dcu-cli.exe /configure -scheduleManual
+
+#https://www.dell.com/support/manuals/en-us/command-update/dellcommandupdate_rg/dell-command-%7C-update-cli-commands?guid=guid-92619086-5f7c-4a05-bce2-0d560c15e8ed&lang=en-us
