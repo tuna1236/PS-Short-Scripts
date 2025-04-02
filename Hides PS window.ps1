a4d2ceca9f57d5f347a463415acfc050ecd04dee
Cole <tuna1236@gmail.com>
2025-04-02 07:16:03 -0700

    Update Hides PS window.ps1

--- Hides PS window.ps1
+++ Hides PS window.ps1
@@ -6,12 +6,6 @@ $Script:showWindowAsync = Add-Type -MemberDefinition @"
 [DllImport("user32.dll")]
 public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
 "@ -Name "Win32ShowWindowAsync" -Namespace Win32Functions -PassThru
-Function Show-Powershell()
-{
-$null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $pid).MainWindowHandle, 10)
-}
-Function Hide-Powershell()
-{
-$null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $pid).MainWindowHandle, 2)
-}
+Function Show-Powershell(){$null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $pid).MainWindowHandle, 10)}
+Function Hide-Powershell(){$null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $pid).MainWindowHandle, 2)}
 Hide-PowerShell
